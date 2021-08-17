764848da65b1a533cea111dec196df1da1816688
Radomír Polách <rp@t4d.cz>
2021-08-17 11:53:01 +0200

    wip2

--- mailtool.py
+++ mailtool.py
@@ -39,7 +39,7 @@ argparser.add_argument('--showpasswords', dest='showpassowords', help="Dry run",
 argparser.add_argument('-a', '--eml1', dest='eml1', nargs="+", help='Append emls')
 argparser.add_argument('-m', '--mbox1', dest='mbox1', help="File location of mbox1", default=False)
 argparser.add_argument('-s', '--host1', dest='host1', help="IMAP host for host1", default=False)
-argparser.add_argument('-p', '--prot1', dest='port1', help="IMAP port for host1", default=1143)
+argparser.add_argument('-p', '--port1', dest='port1', help="IMAP port for host1", default=1143)
 argparser.add_argument('-u', '--user1', dest='username1', help="IMAP username for host1", default='username')
 argparser.add_argument('-P', '--password1', dest='password1', help="IMAP password for host1", default=False)
 argparser.add_argument('--passfile1', dest='passfile1', help="IMAP password file for host1", default=False)
@@ -54,12 +54,13 @@ argparser.add_argument('--prot2', dest='port2', help="IMAP port for host2", defa
 argparser.add_argument('--user2', dest='username2', help="IMAP username for host2", required=False)
 argparser.add_argument('--password2', dest='password2', help="IMAP password for host2", default=False)
 argparser.add_argument('--passfile2', dest='passfile2', help="IMAP password file for host2", default=False)
+argparser.add_argument('--passrequest2', dest='passrequest2', help="IMAP password request for host2", default=False)
 argparser.add_argument('--ssl2', dest='ssl2', help="Use SSL for host2", default=False, action='store_true')
 argparser.add_argument('--gmail2', dest='gmail2', help="Gmail for host2", default=False, action='store_true')
 
 argparser.add_argument('-f', dest='folders', help="IMAP folder", nargs="+")
 
-#argparser.add_argument('-l', dest='local_folder', help="Local folder", default='.')
+argparser.add_argument('-l', dest='list', help="List", default="\"\"")
 argparser.add_argument('-e', '--extended', dest='extended', help="Extended list", default=False, action='store_true')
 argparser.add_argument('--extended2', dest='extended2', help="Extended list", default=False, action='store_true')
 argparser.add_argument('-q', '--query', dest='query', nargs="+", help='Query messages')
@@ -69,34 +70,13 @@ argparser.add_argument('--delete', dest='delete', help='Delete', default=False, 
 
 args = argparser.parse_args()
 
-class Storage():
-    def __init__(self):
-        pass
-    def selectMessageIds():
-        pass
-    def selectMessageById(id):
-        pass
-    def insertMessage(message):
-        pass
-
-class EMLSet(Storage):
-    def __init__(sefl):
-        pass
-
-class IMAPStorage(Storage):
-    def __init__(sefl, imap, folder):
-        pass
-
-class MBOXStorage(Storage):
-    def __init__(sefl):
-        pass
+# Init
 
 if args.passrequest1:
     args.password1 = getpass.getpass()
 
-if args.debug:
-    imaplib.Debug = 4
-    print('Capabilities:', mail.capabilities)
+if args.passrequest2:
+    args.password2 = getpass.getpass()
 
 if args.gmail1:
     args.ssl1 = true
@@ -108,26 +88,57 @@ if args.gmail2:
     args.host2 = 'imap.gmail.com'
     args.port2 = 993
 
+if args.debug:
+    imaplib.Debug = 4
+
 if args.host1:
     if not args.ssl1:
-        mail = imaplib.IMAP4(args.host1, args.port1)
-        mail.starttls()
+        imap1 = imaplib.IMAP4(args.host1, args.port1)
+        imap1.starttls()
     else:
-        mail = imaplib.IMAP4_SSL(args.host1, args.port1)
+        imap1 = imaplib.IMAP4_SSL(args.host1, args.port1)
+    imap1.login(args.username1, args.password1)
+    if args.debug:
+        print('imap1 capabilities:', imap1.capabilities, file=sys.stderr)
 
-    mail.login(args.username1, args.password1)
+if args.host2:
+    if not args.ssl2:
+        imap2 = imaplib.IMAP4(args.host2, args.port2)
+        imap2.starttls()
+    else:
+        imap2 = imaplib.IMAP4_SSL(args.host2, args.port2)
+    imap2.login(args.username2, args.password2)
+    if args.debug:
+        print('imap2 capabilities:', imap2.capabilities, file=sys.stderr)
+    
+if (args.mbox1):
+    mbox1 = mailbox.mbox(args.mbox1)
 
+if (args.mbox2):
+    mbox2 = mailbox.mbox(args.mbox2)
 
-print(" ".join(map(pipes.quote, sys.argv)), file=sys.stderr)
+if args.debug:
+    print(" ".join(map(pipes.quote, sys.argv)), file=sys.stderr)
 
 #UID, FLAGS
+
+# Use cases
 
 def smallhash(s):
     return int(hashlib.sha256(s).hexdigest(), 16) % 10**8 
 
 def imap_delete(imap):
+    #imap.send(b'MOVE 1:* Trash')
+    imap.copy("1:*", 'Trash')
+    imap.select('Trash');
     imap.store("1:*", '+FLAGS', '\\Deleted')
     imap.expunge()
+
+def imap_select(imap, folder):
+    imap.select("\"" + decode_utf7(folder) + "\"")
+
+def imap_append(imap, folder, message, flags = ''):
+    imap.append(folder, flags, imaplib.Time2Internaldate(time.time()), message)
 
 def mbox_add(mbox, message):
     try:
@@ -140,78 +151,76 @@ def mbox_add(mbox, message):
         return 
     mbox.add(message);
 
+def message_print(message):
+    try:
+        timestamp = email.utils.parsedate_to_datetime(message.get('Date')).timestamp()
+    except:
+        timestamp = 0
+    try:
+        message_id = email.header.make_header(email.header.decode_header(message.get('Message-ID')))
+    except:
+        message_id = message.get('Message-ID')
+    try:
+        subject = message.get('Subject')
+        if subject:
+            #subject = _RE_COMBINE_WHITESPACE.sub(" ", subject).strip()
+            subject = subject.replace('\n', ' ').replace('\r', ' ')
+        subject = email.header.make_header(decode_utf7(email.header.decode_header(subject)))
+    except:
+        pass
 
+    #print('%s\t%d\t%s\t%s' % (timestamp, size, message_id, subject))
+    print('%s\t%s\t%s' % (timestamp, message_id, subject), end="\t")
+    
+    if args.extended2:
+        for part in message.walk():
+            if part.get_content_maintype() == 'multipart':
+                continue
+            fileName = part.get_filename()
+            payload = part.get_payload(decode=True)
+            if payload:
+                size = len(payload)
+                hashcode = smallhash(payload)
+            else:
+                size = 0
+                hashcode = 0
+            disposition = part.get('Contnet-Disposition')
+            #print("%d,%s,%s,%d" % (size, disposition, fileName, hashcode), end=";")
+            if fileName:
+                print("%d,%s,%s" % (size, disposition, fileName), end=";")
+            #if part.get('Content-Disposition') is None:
+            #    continue
+            #if part.get('Content-Disposition') == "inline":
+            #    continue
+    print()
+
+
+# Mbox => Mbox
+# Mbox => Imap
+# Mbox => Print
 if (args.mbox1):
-    mbox1 = mailbox.mbox(args.mbox1)
     if (args.mbox2):
-        mbox2 = mailbox.mbox(args.mbox2)
-        for msg_str in mbox1:
-            try:
-                timestamp = email.utils.parsedate_to_datetime(msg_str.get('Date')).timestamp()
-            except:
-                timestamp = 0
-            if args.fromts and timestamp < args.fromts:
-                continue
-            if args.tots and timestamp > args.tots:
-                continue
-            mbox2.add(msg_str);
-        mbox2.close()
+        for message in mbox1:
+            mbox_add(mbox2, message)
+    elif (args.host1):
+        if len(args.folders) != 0:
+            sys.exit("A single target folder needed!")
+        for message in mbox1:
+            imap_append(imap1, folders[0], message)
     else:
-        for msg_str in mbox1:
-            try:
-                timestamp = email.utils.parsedate_to_datetime(msg_str.get('Date')).timestamp()
-            except:
-                timestamp = 0
-            try:
-                message_id = email.header.make_header(email.header.decode_header(msg_str.get('Message-ID')))
-            except:
-                message_id = msg_str.get('Message-ID')
-            try:
-                subject = msg_str.get('Subject')
-                if subject:
-                    #subject = _RE_COMBINE_WHITESPACE.sub(" ", subject).strip()
-                    subject = subject.replace('\n', ' ').replace('\r', ' ')
-                subject = email.header.make_header(decode_utf7(email.header.decode_header(subject)))
-            except:
-                pass
+        for message in mbox1:
+            message_print(message)
 
-            #print('%s\t%d\t%s\t%s' % (timestamp, size, message_id, subject))
-            print('%s\t%s\t%s' % (timestamp, message_id, subject), end="\t")
-            
-            if args.extended2:
-                for part in msg_str.walk():
-                    if part.get_content_maintype() == 'multipart':
-                        continue
-                    fileName = part.get_filename()
-                    payload = part.get_payload(decode=True)
-                    if payload:
-                        size = len(payload)
-                        hashcode = smallhash(payload)
-                    else:
-                        size = 0
-                        hashcode = 0
-                    disposition = part.get('Contnet-Disposition')
-                    #print("%d,%s,%s,%d" % (size, disposition, fileName, hashcode), end=";")
-                    if fileName:
-                        print("%d,%s,%s" % (size, disposition, fileName), end=";")
-                    #if part.get('Content-Disposition') is None:
-                    #    continue
-                    #if part.get('Content-Disposition') == "inline":
-                    #    continue
-            print()
-    mbox1.close();
-
-if args.folders:
+elif args.folders:
     for folder in args.folders:
-        mail.select("\"" + decode_utf7(folder) + "\"")
+        imap_select(imap1, folder)
         if args.delete:
-            mail.store("1:*", '+FLAGS', '\\Deleted')
-            mail.expunge()
+            imap_delete(imap1)
         else:
-            typ, data = mail.search(None, 'ALL')
+            typ, data = imap1.search(None, 'ALL')
             if args.eml2: # dump emls to folder
                 for num in data[0].split():
-                    typ, data = mail.fetch(num, '(UID RFC822)')
+                    typ, data = imap1.fetch(num, '(UID RFC822)')
                     data = parse_fetch_response(data)
                     #f = open('%s/%010d.eml' %(args.local_folder, int(num)), 'w')
                     uid = next(iter(data))
@@ -221,7 +230,7 @@ if args.folders:
             elif args.mbox2: # dump emls to mbox
                 mbox = mailbox.mbox(args.mbox2)
                 for num in data[0].split():
-                    typ, data = mail.fetch(num, '(UID RFC822)')
+                    typ, data = imap1.fetch(num, '(UID RFC822)')
                     data = parse_fetch_response(data)
                     uid = next(iter(data))
                     if b'RFC822' in data[uid]:
@@ -235,22 +244,22 @@ if args.folders:
                 for file in args.eml1:
                     with open(file, "rb") as f:
                         data = f.read()#.decode('utf-8')
-                        mail.append(args.remote_folder, '', imaplib.Time2Internaldate(time.time()), data)
+                        imap_append(imap1, folder, message)
             elif (args.query):
                 queryset = frozenset(args.query)
-                typ, data = mail.search(None, 'ALL')
+                typ, data = imap1.search(None, 'ALL')
                 for num in data[0].split():
-                    typ, data = mail.fetch(num, '(BODY.PEEK[HEADER.FIELDS (MESSAGE-ID)])')
+                    typ, data = imap1.fetch(num, '(BODY.PEEK[HEADER.FIELDS (MESSAGE-ID)])')
                     for response_part in data:
                       if isinstance(response_part, tuple):
-                        msg_str = email.message_from_string(response_part[1].decode('utf-8'))
+                        msg_str = imap1.message_from_string(response_part[1].decode('utf-8'))
                         message_id = msg_str.get('Message-ID')
                         if message_id in queryset:
                             print("match")
-                            typ, data = mail.fetch(num, '(RFC822)')
+                            typ, data = imap1.fetch(num, '(RFC822)')
                             for response_part in data:
                                 if isinstance(response_part, tuple):
-                                    msg_str = email.message_from_string(response_part[1].decode('utf-8'))
+                                    msg_str = imap1.message_from_string(response_part[1].decode('utf-8'))
                                     for part in msg_str.walk():
                                         if part.get_content_maintype() == 'multipart':
                                             continue
@@ -264,51 +273,65 @@ if args.folders:
                 for num in data[0].split():
                     if (args.delete):
                         pass
-                        #mail.store(num, '+FLAGS', '\\Deleted')
+                        #imap1.store(num, '+FLAGS', '\\Deleted')
                     elif (args.extended):
-                        typ, data = mail.fetch(num, '(BODY.PEEK[HEADER.FIELDS (MESSAGE-ID SUBJECT DATE)] RFC822.SIZE)')
-                        #typ, data = mail.fetch(num, '(RFC822)')
+                        typ, data = imap1.fetch(num, '(BODY.PEEK[HEADER.FIELDS (MESSAGE-ID SUBJECT DATE)] RFC822.SIZE)')
+                        #typ, data = imap1.fetch(num, '(RFC822)')
                         #(name, size) = parse_imap_value(data[1].decode('utf-8'))
                         for response_part in data:
                           if isinstance(response_part, tuple):
-                            msg_str = email.message_from_string(response_part[1].decode('utf-8'))
+                            msg_str = imap1.message_from_string(response_part[1].decode('utf-8'))
                             try:
-                                timestamp = email.utils.parsedate_to_datetime(msg_str.get('Date')).timestamp()
+                                timestamp = imap1.utils.parsedate_to_datetime(msg_str.get('Date')).timestamp()
                             except:    
                                 timestamp = 0
                             try:
-                                message_id = email.header.make_header(email.header.decode_header(msg_str.get('Message-ID')))
+                                message_id = imap1.header.make_header(imap1.header.decode_header(msg_str.get('Message-ID')))
                             except:
                                 message_id = msg_str.get('Message-ID')
                             try:
-                                subject = email.header.make_header(decode_utf7(email.header.decode_header(msg_str.get('Subject'))))
+                                subject = imap1.header.make_header(decode_utf7(imap1.header.decode_header(msg_str.get('Subject'))))
                             except:
                                 subject = msg_str.get('Subject')
 
                             #print('%s\t%d\t%s\t%s' % (timestamp, size, message_id, subject))
                             print('%s\t%s\t%s' % (timestamp, message_id, subject))
                     else:
-                        typ, data = mail.fetch(num, '(BODY.PEEK[HEADER.FIELDS (MESSAGE-ID)])')
+                        typ, data = imap1.fetch(num, '(BODY.PEEK[HEADER.FIELDS (MESSAGE-ID)])')
                         for response_part in data:
                           if isinstance(response_part, tuple):
                             msg_str = email.message_from_string(response_part[1].decode('utf-8'))
                             try:
-                                message_id = email.header.make_header(email.header.decode_header(msg_str.get('Message-ID')))
+                                message_id = imap1.header.make_header(imap1.header.decode_header(msg_str.get('Message-ID')))
                             except:
                                 message_id = msg_str.get('Message-ID')
                             print(message_id);
                 if (args.delete):
                     pass
-                    #mail.expunge()
+                    #imap1.expunge()
 
-if args.host1 and not args.folders:
-    for i in mail.list()[1]:
+elif args.host1:
+    #imap1.select("Sent")
+    #imap1.search("UID", None, "2090:2312")
+    #sys.exit()
+    for i in imap1.list(args.list)[1]:
+    #for i in imap1.list('(SPECIAL-USE)', '"" "*"')[1]:
+    #for i in imap1.list('(SPECIAL-USE)', '"" "*"')[1]:
         print(i.decode('utf-8'))
         print(decode_utf7(i))
 
-        #print(imaputf7decode(i.decode('utf-8')))
-        #print(imaputf7encode(imaputf7decode(i.decode('utf-8'))))
+# Cleanup
+
+if args.mbox1:
+    mbox1.close()
+
+if args.mbox2:
+    mbox2.close()
 
 if args.host1:
-    mail.close()
-    mail.logout()
+    #imap1.close()
+    imap1.logout()
+
+if args.host2:
+    imap2.close()
+    imap2.logout()
